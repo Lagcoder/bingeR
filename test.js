e0f403d82292d9fefe94bef49a5024487741e0d4
Lagcoder <262425898+Lagcoder@users.noreply.github.com>
2026-09-12 09:37:43 +0100

    Remove service parameter from fetch request

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const WORKER_URL = 'https://apisecure.lagcoder.workers.dev';
 
 async function testConnection() {
   try {
-    const response = await fetch(`${WORKER_URL}?service=movie&q=Inception`);
+    const response = await fetch(`${WORKER_URL}?q=Inception`);
     const data = await response.json();
     console.log('Worker is working securely:', data);
   } catch (error) {
